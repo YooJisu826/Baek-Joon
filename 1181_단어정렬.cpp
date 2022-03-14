4052294ac6d118cbfb2e6b5db6856c923ec92383
유지수 <71329329+YooJisu826@users.noreply.github.com>
2022-03-15 00:00:23 +0900

    Solved 1181
    실버5

---
+++ 1181_단어정렬.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(string a, string b) {
+	//길이가 같으면 사전순
+	if (a.length() == b.length()) {
+		return a < b;
+	}
+	//길이 순
+	else {
+		return a.length() < b.length();
+	}
+}
+
+int main() {
+
+	cin.tie(NULL); ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vector<string> word;
+	string temp;
+	for (int i = 0; i < n; i++)
+		cin >> word[i];
+
+	sort(word.begin(), word.end(), compare);
+
+	for (int i = 0; i < n; i++) {
+		if (temp == word[i]) continue;
+		cout << word[i] << '\n';
+		temp = word[i];
+	}
+
+
+
+}
